1efe6052b8657481390d4cadbf83c727080a2f95
Mihary Joel <miharyjoel@gmail.com>
2023-02-06 23:37:13 +0300

    feat: todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,13 @@
-import './App.css';
-import {useState} from "react";
+import "./App.css";
+import AddTodo from "./component/AddToDo";
 
 function App() {
-    const [todos, setTodos] = useState([]);
-    const [done, setDone] = useState([]);
-    const [newTodo, setNewTodo] = useState("");
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        setTodos([...todos, newTodo]);
-        setNewTodo("");
-    };
-
-    const handleCheck = index => {
-        setDone([...done, todos[index]]);
-        setTodos(todos.filter((_, i) => i !== index));
-    };
-
-    return (
-        <div className="todo-list">
-            <h1> TODO LIST</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={newTodo}
-                    onChange={event => setNewTodo(event.target.value)}
-                />
-            </form>
-            <div className="todo-lists">
-                <div className="todo-column">
-                    <div className="title">
-                        <h3>Todo</h3>
-                    </div>
-                    <ul className="liste">
-                        {todos.map((todo, index) => (
-                            <li key={index} >
-                                <input type="checkbox" onClick={() => handleCheck(index)} />
-                                {todo}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="done-column">
-                    <div className="title">
-                        <h3>Done</h3>
-                    </div>
-                    <ul className="liste">
-                        {done.map((todo, index) => (
-                            <li key={index}>{todo}</li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="todo-list">
+      <h1> TODO LIST</h1>
+      <AddTodo />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/AddToDo.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+const AddTodo = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [done, setDone] = useState([]);
+
+  const handleCheck = (index) => {
+    setDone([...done, todos[index]]);
+    setTodos(todos.filter((todo, i) => i !== index));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  };
+  const updateInputVal = (e) => setNewTodo(e.target.value);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          aria-label="Add Todo"
+          type="text"
+          required
+          value={newTodo}
+          onChange={updateInputVal}
+        />
+      </form>
+      <div className="todo-lists">
+        <div className="todo-column">
+          <div className="title">
+            <h3>Todo</h3>
+          </div>
+          <div className="scroll">
+            <ul className="liste">
+              {todos.map((todo, index) => (
+                <li key={index}>
+                  <input
+                    type="checkbox"
+                    aria-checked="false"
+                    onClick={() => handleCheck(index)}
+                  />
+                  {todo}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="done-column">
+          <div className="title">
+            <h3>Done</h3>
+          </div>
+          <div className="scroll">
+            <ul className="liste">
+              {done.map((todo, index) => (
+                <li key={index}>{todo}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddTodo;
